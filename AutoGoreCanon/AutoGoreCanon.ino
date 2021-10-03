c1752e98474e00fe68d093297c69337030a0e066
JamesM92 <36548759+JamesM92@users.noreply.github.com>
2021-10-03 11:06:17 -0500

    Update AutoGoreCanon.ino
    James Manley - added on.Expired checks or all the timers int he setup script to prevent un-intended triggers on bootup.

--- AutoGoreCanon/AutoGoreCanon.ino
+++ AutoGoreCanon/AutoGoreCanon.ino
@@ -26,19 +26,20 @@ Last updated: 2021/09/30
 int pumpPot = A0; // potentiometer that adjusts the water pump time.
 
 //outputs
-int air_relay = 2;
-int pump_relay = 3;
+int air_relay = 3;
+int pump_relay = 2;
 int audio_out = 4; // output to audio controller device. 
+int LED_Armed = 13;
 
 //buttons
-RBD::Button trigger = 11; can either have 11 be a button 
+RBD::Button trigger = 11; //can either have 11 be a button 
 
 //timers
 RBD::Timer air; // how long the air will cycle for
 RBD::Timer pause; // how long it will wait before starting the pump
 RBD::Timer dispense; // how long the water pump will cycle for
 RBD::Timer standby; //lock the system from being triggered again for a given amount of time
-RBd::Timer audio; //used to stop trigger for audio if needed
+RBD::Timer audio; //used to stop trigger for audio if needed
 
 //global variables
 int potRead = 0;
@@ -50,16 +51,29 @@ void setup() {
   pinMode(air_relay, OUTPUT);
   pinMode(pump_relay, OUTPUT);
   pinMode(pumpPot, INPUT);
-  air.setTimeout(300); // set for air to trigger for 0.3 seconds
-  pause.setTimeout(500); // set to wait for 0.5 second after canon has stopped to start pumping water
+  air.setTimeout(1500); // set for air to trigger for 0.3 seconds
+  if(air.onExpired()){ //need to make sure the on.Expired value is called beore entiring main program.
+    delay(1);
+  }
+  pause.setTimeout(2000); // set to wait for 0.5 second after canon has stopped to start pumping water
+  if(pause.onExpired()){ //need to make sure the on.Expired value is called beore entiring main program.
+    delay(1);
+  }
   dispense.setTimeout(5000); // dispense time will be set when the trigger button is pressed 
+  if(dispense.onExpired()){ //need to make sure the on.Expired value is called beore entiring main program.
+    delay(1);
+  }
   standby.setTimeout(30000); // sets the system to standby for 30 seconds after last trigger 
   standby.restart(); // puts the system in 30 second standby on boot
+  while (standby.isActive()){ //need to initiatilize all the timers
+    delay(1);    
+  }
 }
 
 void loop() {
   if(standby.onExpired()){
     Serial.println("Standby time has elapsed");
+    digitalWrite(LED_Armed, HIGH);
   }
   
   if((standby.isActive()) && (trigger.onPressed())){
@@ -69,25 +83,27 @@ void loop() {
   if((standby.isExpired()) && trigger.onPressed()){
     Serial.println("button was pressed");
     Serial.println("air was triggered");
+    digitalWrite(LED_Armed, LOW);
     digitalWrite(air_relay, HIGH);
     digitalWrite(audio_out, HIGH);
-    potRead = analogRead(pumpPot);
+    //potRead = analogRead(pumpPot);
+    potRead = 800; //not using potentiometer, just setting value in code.
     air.restart();
-    standby.restart(); // standby wait time starts when the canon was triggered
+    
   }
 
-  if(audio.onExpired()){
+  if( audio.onExpired()){
     digitalWrite(audio_out, LOW);
   }
 
-  if(air.onExpired()){
+  if( air.onExpired()){
     digitalWrite(air_relay, LOW);
     Serial.println("Air was turned off");
     Serial.println("waiting before turning on pump");
     pause.restart();
   }
 
-  if(pause.onExpired()){
+  if( pause.onExpired()){
     digitalWrite(pump_relay, HIGH);
     Serial.println("Pump has been turned on");
     pumptime = map(potRead, 0, 1023, 1000, 10000); // map (input, input_low, input_high, output_low, output_high)
@@ -96,10 +112,11 @@ void loop() {
     dispense.restart();
   }
 
-  if(dispense.onExpired()){
+  if( dispense.onExpired()){
     digitalWrite(pump_relay, LOW);
     Serial.println("Pump has been turned off");
-    Serial.println("System is now on standby")
+    Serial.println("System is now on standby");
+    standby.restart(); // standby wait time starts when the canon was triggered
   }
 
 
